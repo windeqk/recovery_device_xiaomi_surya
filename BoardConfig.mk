95d7f8826e9af05695e80f244c418fce33840b39
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-03-26 02:08:31 -0600

    surya: Set TARGET_USE_CUSTOM_LUN_FILE_PATH
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>
    Change-Id: I21b157da329a11d58260d6e74854ed88e718fa31

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,3 +90,6 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+
+# Team Win Recovery - USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
